11187154e91344aedc56a2088eaf0c6d8ee25f60
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2020-08-09 17:17:52 +0200

    Add wasm example

--- examples/editor2/main.go
+++ examples/editor2/main.go
@@ -14,8 +14,52 @@ import (
 	"math"
 )
 
+func dotExample() (*particles.ParticleSystem, UI) {
+	g := NewGame(2.5, 0.9, 0, 1.3, 0, 0, 0)
+	//g.ui.exampleIndex = 0
+
+	g.particles.Material = particles.MaterialDot
+	//g.ui.materialIndex = 2
+
+	c1 := [4]float32{1, 1, 1, 1}
+	c2 := [4]float32{1, 1, 1, 0}
+	g.particles.ColorOverLifetime = newColorOverLifetime(c1, c2)
+	g.ui.colorOverLifetime = ColorOverLifetime{
+		enabled:    true,
+		startColor: c1,
+		endColor:   c2,
+	}
+
+	g.particles.SizeOverLifetime = newSizeOverLifetime(0, 1.5)
+	g.ui.sizeOverLifeTime = SizeOverLifetime{
+		enabled: true,
+		start:   0,
+		end:     1.8,
+	}
+	return g.particles, g.ui
+}
+
+func fireExample() (*particles.ParticleSystem, UI) {
+	g := NewGame(2, 0.2, 5, 700, 8, 75, 2.5)
+	//g.ui.exampleIndex = 1
+
+	g.particles.Material = particles.MaterialDot
+	//g.ui.materialIndex = 2
+
+	c1 := [4]float32{1, 0.25, 0, 1}
+	c2 := [4]float32{1, 1, 0, 0}
+	g.particles.ColorOverLifetime = newColorOverLifetime(c1, c2)
+	g.ui.colorOverLifetime = ColorOverLifetime{
+		enabled:    true,
+		startColor: c1,
+		endColor:   c2,
+	}
+
+	return g.particles, g.ui
+}
+
 type UI struct {
-	rate  float32
+	rate float32
 
 	colorOverLifetime ColorOverLifetime
 	sizeOverLifeTime  SizeOverLifetime
--- go.mod
+++ go.mod
@@ -5,13 +5,16 @@ go 1.14
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabstv/ebiten-imgui v0.0.6
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200707082815-5321531c36a2 // indirect
 	github.com/hajimehoshi/ebiten v1.12.0-alpha.1.0.20200514190429-f7f507e912dc
 	github.com/inkyblackness/imgui-go/v2 v2.4.1
-	github.com/kyeett/guigi v0.0.0-00010101000000-000000000000
+	github.com/kyeett/guigi v0.1.2 // indirect
 	github.com/peterhellberg/gfx v0.0.0-20200413155834-f94ca2597d91
 	github.com/stretchr/testify v1.6.0
+	golang.org/x/exp v0.0.0-20200513190911-00229845015e // indirect
 	golang.org/x/image v0.0.0-20200801110659-972c09e46d76
+	golang.org/x/mobile v0.0.0-20200801112145-973feb4309de // indirect
+	golang.org/x/sys v0.0.0-20200808120158-1030fc2bf1d9 // indirect
 )
 
 // replace github.com/kyeett/guigi => ../guigi
-// replace github.com/kyeett/games => ../games
